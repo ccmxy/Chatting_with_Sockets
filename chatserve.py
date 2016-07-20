378c63eb95ab423137ba2874e46b8a99e6fd7b88
Ubuntu <ubuntu@ip-172-31-45-57.us-west-2.compute.internal>
2016-07-20 20:37:35 +0000

    Removed asterisk clutter

--- chatserve.py
+++ chatserve.py
@@ -12,32 +12,32 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     client.
     """
     def handle(self):
-	print "Connection with new chatter established! Querying for name...";
-        server_msg = "What is your name?\n";
-        sent = self.request.send(server_msg);
+	print "Connection with new chatter established! Querying for name..."
+        server_msg = "What is your name?\n"
+        sent = self.request.send(server_msg)
 	self.client_response = self.request.recv(500).split("\n", 1)[0]
         client_name = self.client_response
         quit = 0
         while quit < 1:
-            print client_name + "> " + self.client_response;
+            print client_name + "> " + self.client_response
            # if client sends \quit, break loop  
             if (self.client_response == "\quit"):
                 quit = 1
                 print "Connection with " + client_name + " closed by client."     
                 break
-            sys.stdout.write(' You> ');
-            sys.stdout.flush(); 
+            sys.stdout.write(' You> ')
+            sys.stdout.flush()
             server_msg = stdin.readline()
            # self.request is the TCP socket connected to the client 
-            self.request.send(server_msg);
+            self.request.send(server_msg)
            # if server sends \quit, close connection with client and break loop
             if(server_msg == "\quit\n"):
-                quit = 1;
+                quit = 1
                 self.request.close()
                 print "You have ended the connection with " + client_name 
                 break
             print "Awaiting response from client..."
-            self.client_response = self.request.recv(500).split("\n",1)[0];
+            self.client_response = self.request.recv(500).split("\n",1)[0]
         print "Listening for connection..."
 
 
